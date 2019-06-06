13d2949538e8b1dffc4caa1b212d1c03f6bb0660
kazumalab <kazuma-arashi@hotmail.co.jp>
2019-06-06 19:51:05 +0900

    change file name

--- migrate/create_tables.rb
+++ migrate/create_tables.rb
@@ -4,7 +4,9 @@ config = YAML.load_file("#{File.dirname(__FILE__)}/../config/database.yml")
 ActiveRecord::Base.establish_connection(config['development'])
 connection = ActiveRecord::Base.connection
 
-connection.create_table :items do |t|
+connection.drop_table :items, if_exists: true
+
+connection.create_table :items, force: true do |t|
   t.string :name, null: false
   t.integer :state, null: false, default: 0
 end
