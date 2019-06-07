fffd151a07bfa06081e5e8fb617f0b592ae4ef2c
kazumalab <kazuma-arashi@hotmail.co.jp>
2019-06-07 11:46:52 +0900

    add: category and relation model

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require "active_record"
-require_relative "./models/item"
+Dir["./models/*.rb"].each {|file| require file }
 
 config = YAML.load_file("#{File.dirname(__FILE__)}/config/database.yml")
 ActiveRecord::Base.establish_connection(config['development'])
 
 items = Item.all
+categories = Category.all
 puts items.count
+puts categories.count
--- migrate/create_tables.rb
+++ migrate/create_tables.rb
@@ -5,8 +5,19 @@ ActiveRecord::Base.establish_connection(config['development'])
 connection = ActiveRecord::Base.connection
 
 connection.drop_table :items, if_exists: true
+connection.drop_table :categories, if_exists: true
+connection.drop_table :item_categories, if_exists: true
 
 connection.create_table :items, force: true do |t|
   t.string :name, null: false
   t.integer :state, null: false, default: 0
+end
+
+connection.create_table :categories, force: true do |t|
+  t.string :name, null: false
+end
+
+connection.create_table :item_categories, force: true do |t|
+  t.integer :item_id, null: false
+  t.integer :category_id, null: false
 end
