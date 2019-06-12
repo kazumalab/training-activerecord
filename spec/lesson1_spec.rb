ab4f2872a1be5fb5758f0e1cbb5ca8ab15cc53e5
kazumalab <kazuma-arashi@hotmail.co.jp>
2019-06-12 16:25:36 +0900

    add relation

--- spec/lesson1_spec.rb
+++ spec/lesson1_spec.rb
@@ -1,11 +1,19 @@
 require "spec_helper"
 
 describe "Lesson1" do
-  context "Create Item" do
+  context "アイテムを一つ生成する" do
     before do
       Item.create(name: "お菓子")
     end
 
     it { expect(Item.count).to eq 1 }
+  end
+
+  context "アイテムを生成するが、nameが空" do
+    before do
+      Item.create(name: "")
+    end
+
+    it { expect(Item.count).to eq 0 }
   end
 end
