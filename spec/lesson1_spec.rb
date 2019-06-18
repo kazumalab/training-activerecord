b91dde9a4e48c58224a5b633170cd217bcc526c3
kazumalab <kazuma-arashi@hotmail.co.jp>
2019-06-18 19:46:32 +0900

    add: test case

--- spec/lesson1_spec.rb
+++ spec/lesson1_spec.rb
@@ -1,6 +1,58 @@
 require "spec_helper"
 
 describe "Lesson1" do
+  it do
+    category_choco = Category.create(name: "チョコレート")
+    begin
+      ActiveRecord::Base.transaction do
+        ["チロルチョコ", "キットカット", "アルフォート", nil].each do |item_name|
+          category_choco.items.create!(name: item_name)
+        end
+      end
+    rescue
+      expect(Item.count).to eq 0
+    end
+  end
+
+  it do
+    category_choco = Category.create(name: "チョコレート")
+    ["チロルチョコ", "キットカット", "アルフォート", nil].each do |item_name|
+      category_choco.items.create(name: item_name)
+    end
+
+    expect(Item.count).to eq 3
+  end
+
+  it do
+    category_choco = Category.create(name: "チョコレート")
+    ["チロルチョコ", "キットカット", "アルフォート", nil].each do |item_name|
+      category_choco.items.new(name: item_name)
+    end
+    category_choco.save
+
+    expect(Item.count).to eq 0
+  end
+
+  it "リレーションが貼られている場合は全てrollbackされる" do
+    category_choco = Category.create(name: "チョコレート")
+    ["チロルチョコ", "キットカット", "アルフォート", nil].each do |item_name|
+      category_choco.items.new(name: item_name)
+    end
+
+    category_choco.save
+
+    expect(Item.count).to eq 0
+  end
+
+  it "リレーションが貼られている場合でもcreateを使う場合は対象のものだけRollbackされる" do
+    category_choco = Category.create(name: "チョコレート")
+    ["チロルチョコ", "キットカット", "アルフォート", nil].each do |item_name|
+      category_choco.items.create(name: item_name)
+    end
+
+    expect(Item.count).to eq 3
+  end
+
   it "リレーション先からデータを取得" do
     # チョコレートカテゴリ
     category_choco = Category.create(name: "チョコレート")
@@ -17,13 +69,11 @@ describe "Lesson1" do
     items = Item.all
     items.each do |item|
       category = item.category
-      puts category.name
     end
 
     items = Item.includes(:category).all
     items.each do |item|
       category = item.category
-      puts category.name
     end
   end
 end
