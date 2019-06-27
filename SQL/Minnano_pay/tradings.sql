d4918cbd42faa36cbb4dc4ea051a511365782412
Ayaka Hanaoka <aya.yb.0427@gmail.com>
2019-06-27 12:11:46 +0900

    タイポ直した

--- SQL/Minnano_pay/tradings.sql
+++ SQL/Minnano_pay/tradings.sql
@@ -2,5 +2,5 @@ CREATE TABLE tradings (
   id int PRIMARY KEY,
   amount int,
   pay_account_id int REFERENCES pay_accounts,
-  transfer_type_id int REFERENCES transfer_type
+  transfer_type_id int REFERENCES transfer_types
 );
